ea0c7f3b7543e97deb3748f2a33ef5d49b6e1454
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-06-03 12:15:14 +0100

    fix: adjust pooling layer depending on context

--- job_scripts/array_jobs.sh
+++ job_scripts/array_jobs.sh
@@ -2,7 +2,7 @@
 
 #PBS -l select=1:ncpus=4:mem=4gb
 #PBS -l walltime=4:00:00
-#PBS -N tlearners_SSRIstop
+#PBS -N tlearners_SSRIstop_and_BDIonly
 #PBS -J 0-35
 
 # Load environment
@@ -11,21 +11,36 @@ source activate graphtrip
 cd ~/projects/graphTRIP/experiments
 
 # Define job ranges for each script
-PSILOCYBIN_TLEARNER_START=0
-PSILOCYBIN_TLEARNER_END=17
-ESCITALOPRAM_TLEARNER_START=18
-ESCITALOPRAM_TLEARNER_END=35
+TLEARNER_P_SSRIstop_START=0
+TLEARNER_P_SSRIstop_END=8
+TLEARNER_E_SSRIstop_START=9
+TLEARNER_E_SSRIstop_END=17
+
+TLEARNER_P_BDIonly_START=18
+TLEARNER_P_BDIonly_END=26
+TLEARNER_E_BDIonly_START=27
+TLEARNER_E_BDIonly_END=35
 
 # Get the current job index
 JOB_ID=${PBS_ARRAY_INDEX}
 
 # Run the appropriate script based on the job index
-if [ $JOB_ID -ge $PSILOCYBIN_TLEARNER_START ] && [ $JOB_ID -le $PSILOCYBIN_TLEARNER_END ]; then
-    # Psilocybin T-learner (3 jobs; 0-2)
-    python run_experiment.py train_tlearner NeptuneObserver --jobid=$JOB_ID --config_json='tlearner_P_SSRIstop.json'
-elif [ $JOB_ID -ge $ESCITALOPRAM_TLEARNER_START ] && [ $JOB_ID -le $ESCITALOPRAM_TLEARNER_END ]; then
-    # Escitalopram T-learner (3 jobs; 3-5)
-    python run_experiment.py train_tlearner NeptuneObserver --jobid=$((JOB_ID - ESCITALOPRAM_TLEARNER_START)) --config_json='tlearner_E_SSRIstop.json'
+if [ $JOB_ID -ge $TLEARNER_P_SSRIstop_START ] && [ $JOB_ID -le $TLEARNER_P_SSRIstop_END ]; then
+    # Psilocybin T-learner SSRIstop (0-8)
+    python run_experiment.py train_tlearner NeptuneObserver --jobid=$((JOB_ID - TLEARNER_P_SSRIstop_START)) --config_json='tlearner_P_SSRIstop.json'
+
+elif [ $JOB_ID -ge $TLEARNER_E_SSRIstop_START ] && [ $JOB_ID -le $TLEARNER_E_SSRIstop_END ]; then
+    # Escitalopram T-learner SSRIstop (9-17)
+    python run_experiment.py train_tlearner NeptuneObserver --jobid=$((JOB_ID - TLEARNER_E_SSRIstop_START)) --config_json='tlearner_E_SSRIstop.json'
+
+elif [ $JOB_ID -ge $TLEARNER_P_BDIonly_START ] && [ $JOB_ID -le $TLEARNER_P_BDIonly_END ]; then
+    # Psilocybin T-learner BDIonly (18-26)
+    python run_experiment.py train_tlearner NeptuneObserver --jobid=$((JOB_ID - TLEARNER_P_BDIonly_START)) --config_json='tlearner_P_BDIonly.json'
+
+elif [ $JOB_ID -ge $TLEARNER_E_BDIonly_START ] && [ $JOB_ID -le $TLEARNER_E_BDIonly_END ]; then
+    # Escitalopram T-learner BDIonly (27-35)
+    python run_experiment.py train_tlearner NeptuneObserver --jobid=$((JOB_ID - TLEARNER_E_BDIonly_START)) --config_json='tlearner_E_BDIonly.json'
+
 else
     echo "Invalid job index: $JOB_ID"
     exit 1
