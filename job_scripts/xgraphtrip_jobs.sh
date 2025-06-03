6083c02d57d4449153e5656c35e812c55905e061
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-06-04 00:21:43 +0100

    fix: bash is hard

--- job_scripts/xgraphtrip_jobs.sh
+++ job_scripts/xgraphtrip_jobs.sh
@@ -10,19 +10,15 @@ module load anaconda3/personal
 source activate graphtrip
 cd ~/projects/graphTRIP/scripts
 
-# Run selected jobs from the config file
-CONFIG_IDS=(185 199 291 0 263 37)
-
 # Get the current job index
 JOB_ID=${PBS_ARRAY_INDEX}
 
-# Calculate which config_id and jobid to use
-CONFIG_INDEX=89 #$((JOB_ID / 42))
+# Calculate which jobid to use
 JOB_INDEX=$((JOB_ID % 42))
 
 # Define the config file and output directory
 CONFIG_FILE='experiments/configs/x_graphtrip_qids.json'
 OUTPUT_DIR='outputs/x_graphtrip_qids/'
 
-# Run x_graphtrip script with the calculated config_id and jobid
-python x_graphtrip.py --jobid=$JOB_INDEX --config_id=${CONFIG_IDS[$CONFIG_INDEX]} --config_file=$CONFIG_FILE --output_dir=$OUTPUT_DIR
+# Run x_graphtrip script with config_id 89 and the calculated jobid
+python x_graphtrip.py --jobid=$JOB_INDEX --config_id=89 --config_file=$CONFIG_FILE --output_dir=$OUTPUT_DIR
