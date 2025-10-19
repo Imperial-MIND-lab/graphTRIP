7fd57ce38a36eb5bafc98a2e54db48f6f2b56b13
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-10-19 11:26:04 +0100

    re-run with different seed

--- job_scripts/screen_tfmpool.sh
+++ job_scripts/screen_tfmpool.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=8:00:00
-#PBS -N screen_tfmpool
-#PBS -J 0-1023
+#PBS -l walltime=6:00:00
+#PBS -N screen_tfmpool_seed1
+#PBS -J 0-511
 
 module load anaconda3/personal
-source activate graphtrp
+source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
-python run_experiment.py train_jointly NeptuneObserver --jobid=${PBS_ARRAY_INDEX} --config_json='screen_tfmpool.json'
+python run_experiment.py train_jointly NeptuneObserver --jobid=${PBS_ARRAY_INDEX} --config_json='screen_tfmpool_seed1.json'
