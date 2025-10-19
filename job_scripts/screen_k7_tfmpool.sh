a36e108b4c769ae23a61911cdfd2a90dd831e53e
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-10-19 22:52:00 +0100

    job for new seed runs

--- job_scripts/screen_k7_tfmpool.sh
+++ job_scripts/screen_k7_tfmpool.sh
@@ -2,11 +2,11 @@
 
 #PBS -l select=1:ncpus=4:mem=4gb
 #PBS -l walltime=6:00:00
-#PBS -N screen_k7_tfmpool_seed0
+#PBS -N screen_k7_tfmpool_seed1
 #PBS -J 0-1535
 
 module load anaconda3/personal
 source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
-python run_experiment.py train_jointly NeptuneObserver --jobid=${PBS_ARRAY_INDEX} --config_json='screen_tfmpool_k7_seed0.json'
+python run_experiment.py train_jointly NeptuneObserver --jobid=${PBS_ARRAY_INDEX} --config_json='screen_tfmpool_k7_seed1.json'
