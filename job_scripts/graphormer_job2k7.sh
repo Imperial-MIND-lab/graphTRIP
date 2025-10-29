bb69d6c7595aed2b62460982209a614fdcdb750c
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-10-29 02:02:07 +0000

    save weights

--- job_scripts/graphormer_job2k7.sh
+++ job_scripts/graphormer_job2k7.sh
@@ -2,11 +2,11 @@
 
 #PBS -l select=1:ncpus=4:mem=4gb
 #PBS -l walltime=2:00:00
-#PBS -N graphormer_job49k7
+#PBS -N graphormer_job2k7
 #PBS -J 0-9
 
 module load anaconda3/personal
 source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
-python run_experiment.py train_jointly NeptuneObserver --jobid=${PBS_ARRAY_INDEX} --config_json='graphormer_job49k7.json'
+python run_experiment.py train_jointly NeptuneObserver --jobid=${PBS_ARRAY_INDEX} --config_json='graphormer_job2k7.json'
