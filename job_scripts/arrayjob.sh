2af5cc7252eb5ecf7c803213fb1221bf66f882a9
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-10-31 18:22:45 +0000

    graphormer finetuning on psilodep1

--- job_scripts/arrayjob.sh
+++ job_scripts/arrayjob.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=2:00:00
-#PBS -N transfer_vgae_retrain_mlp
-#PBS -J 0-9
+#PBS -l walltime=8:00:00
+#PBS -N graphormer_finetuning
+#PBS -J 0-47
 
 module load anaconda3/personal
 source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
-python run_experiment.py transfer_vgae_retrain_mlp FileStorageObserver --jobid=${PBS_ARRAY_INDEX} --config_json='psilodep1_train_mlp.json'
+python run_experiment.py transfer_and_finetune NeptuneObserver --jobid=${PBS_ARRAY_INDEX} --config_json='graphormer_finetuning.json'
