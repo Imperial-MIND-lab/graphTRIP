100b1be26bb19e77c40753beb63d127ffe9fc1d4
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-11-05 21:31:59 +0000

    train to predict longterm qids

---
+++ job_scripts/longterm_2m.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=4:mem=4gb
+#PBS -l walltime=4:00:00
+#PBS -N longterm_2m
+#PBS -J 0-9
+
+module load anaconda3/personal
+source activate graphtrip
+
+cd ~/projects/graphTRIP/experiments
+python run_experiment.py train_jointly FileStorageObserver --jobid=${PBS_ARRAY_INDEX} --config_json='graphtrip_2m.json'
---
+++ job_scripts/longterm_5m.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=4:mem=4gb
+#PBS -l walltime=4:00:00
+#PBS -N longterm_5m
+#PBS -J 0-9
+
+module load anaconda3/personal
+source activate graphtrip
+
+cd ~/projects/graphTRIP/experiments
+python run_experiment.py train_jointly FileStorageObserver --jobid=${PBS_ARRAY_INDEX} --config_json='graphtrip_5m.json'
