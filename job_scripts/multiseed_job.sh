debb2aede0c2b89d355d5fe0a3f6057a3e12eec0
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-11-28 23:50:10 +0000

    increase vgae slightly to get better reconstructions

--- job_scripts/multiseed_job.sh
+++ job_scripts/multiseed_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=3:00:00
-#PBS -N reduce_complexity
-#PBS -J 0-767 
+#PBS -l walltime=4:00:00
+#PBS -N reduce_complexity_2
+#PBS -J 0-319
 
 module load anaconda3/personal
 source activate graphtrip
@@ -13,9 +13,9 @@ cd ~/projects/graphTRIP/experiments
 # Settings
 OBSERVER='NeptuneObserver'
 EXNAME='train_jointly'
-CONFIG_JSON='reduce_complexity.json'
-SEEDS=(0 1 2)
-MAX_NUM_CONFIGS=256
+CONFIG_JSON='reduce_complexity_2.json'
+SEEDS=(0 1 2 3 4 5 6 7 8 9)
+MAX_NUM_CONFIGS=32
 
 # Calculate config index (jobid) and seed index from PBS_ARRAY_INDEX
 # For example, with 288 configs and 3 seeds: PBS_ARRAY_INDEX 0-863 maps to:
