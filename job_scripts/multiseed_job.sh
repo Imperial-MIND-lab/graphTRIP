fd13b33bb436dc5ac989c02a03ad1fec5bc67e66
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-11-28 13:17:57 +0000

    use stratified kfold splits

--- job_scripts/multiseed_job.sh
+++ job_scripts/multiseed_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=6:00:00
+#PBS -l walltime=3:00:00
 #PBS -N reduce_complexity
-#PBS -J 0-863 
+#PBS -J 0-767 
 
 module load anaconda3/personal
 source activate graphtrip
@@ -15,7 +15,7 @@ OBSERVER='NeptuneObserver'
 EXNAME='train_jointly'
 CONFIG_JSON='reduce_complexity.json'
 SEEDS=(0 1 2)
-MAX_NUM_CONFIGS=288
+MAX_NUM_CONFIGS=256
 
 # Calculate config index (jobid) and seed index from PBS_ARRAY_INDEX
 # For example, with 288 configs and 3 seeds: PBS_ARRAY_INDEX 0-863 maps to:
