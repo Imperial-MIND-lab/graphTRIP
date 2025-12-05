ea207012d50a376b667459762b1c23aa92033e8b
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-12-05 11:22:39 +0000

    shorter training

--- job_scripts/simple_arrayjob.sh
+++ job_scripts/simple_arrayjob.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=2:00:00
-#PBS -N pretrain_mlp
-#PBS -J 0-3
+#PBS -l walltime=6:00:00
+#PBS -N transfer_vgae_and_pooling_200epochs
+#PBS -J 0-2
 
 module load anaconda3/personal
 source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
 
-CONFIG_JSON='pretrain_mlp.json'
-OBSERVER='FileStorageObserver'
-EXNAME='retrain_mlp'
+CONFIG_JSON='transfer_vgae_and_pooling.json'
+OBSERVER='NeptuneObserver'
+EXNAME='transfer_vgae'
 
 python run_experiment.py ${EXNAME} ${OBSERVER} --jobid=${PBS_ARRAY_INDEX} --config_json=${CONFIG_JSON}
