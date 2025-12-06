6ee772bf92af2d0c9f297d6f28f30ae273a2f038
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-12-06 13:09:07 +0000

    finetuning

--- job_scripts/arrayjob.sh
+++ job_scripts/arrayjob.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=8:00:00
-#PBS -N transfer_vgae_screening
-#PBS -J 0-159
+#PBS -l walltime=6:00:00
+#PBS -N finetune_on_psilodep1
+#PBS -J 0-251
 
 module load anaconda3/personal
 source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
 
-NUM_CONFIGS_FIRST_GROUP=80
+NUM_CONFIGS_FIRST_GROUP=36
 
 if [ ${PBS_ARRAY_INDEX} -lt $NUM_CONFIGS_FIRST_GROUP ]; then
-    CONFIG_JSON='transfer_vgae_more_graphattrs.json'
+    CONFIG_JSON='finetune_option_b.json'
     JOBID=${PBS_ARRAY_INDEX}
 else
-    CONFIG_JSON='transfer_vgae_and_pooling.json'
+    CONFIG_JSON='finetune_option_c.json'
     JOBID=$((PBS_ARRAY_INDEX - $NUM_CONFIGS_FIRST_GROUP))
 fi
 
