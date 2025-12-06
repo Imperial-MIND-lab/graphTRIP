392b538beeae2814bcd950c8cb39345a675a865a
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-12-06 11:43:40 +0000

    mlp pretraining with all seeds

--- job_scripts/simple_arrayjob.sh
+++ job_scripts/simple_arrayjob.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=8:00:00
-#PBS -N transfer_to_psilodep1_std01
-#PBS -J 0-5
+#PBS -l walltime=6:00:00
+#PBS -N pretrain_mlp
+#PBS -J 0-59
 
 module load anaconda3/personal
 source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
 
-CONFIG_JSON='transfer_to_psilodep1_std01.json'
-OBSERVER='NeptuneObserver'
-EXNAME='transfer_and_finetune'
+CONFIG_JSON='pretrain_mlp.json'
+OBSERVER='FileStorageObserver'
+EXNAME='retrain_mlp'
 
 python run_experiment.py ${EXNAME} ${OBSERVER} --jobid=${PBS_ARRAY_INDEX} --config_json=${CONFIG_JSON}
