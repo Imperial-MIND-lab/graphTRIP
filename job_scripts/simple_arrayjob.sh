88239d3cd278d64424056548beaadcaede9d8504
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-12-07 15:05:02 +0000

    re-start jobs

--- job_scripts/simple_arrayjob.sh
+++ job_scripts/simple_arrayjob.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=6:00:00
-#PBS -N pretrain_mlp
+#PBS -l walltime=8:00:00
+#PBS -N transfer_vgae_option_2
 #PBS -J 0-59
 
 module load anaconda3/personal
@@ -10,8 +10,8 @@ source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
 
-CONFIG_JSON='pretrain_mlp.json'
-OBSERVER='FileStorageObserver'
-EXNAME='retrain_mlp'
+CONFIG_JSON='transfer_vgae_option_2.json'
+OBSERVER='NeptuneObserver'
+EXNAME='transfer_vgae'
 
-python run_experiment.py ${EXNAME} ${OBSERVER} --jobid=${PBS_ARRAY_INDEX} --config_json=${CONFIG_JSON}
+python run_experiment.py ${EXNAME} ${OBSERVER} --jobid=${PBS_ARRAY_INDEX} --config_json=${CONFIG_JSON} --seed=0
