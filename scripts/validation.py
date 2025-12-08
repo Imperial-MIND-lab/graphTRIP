9183992fb97f3cfeb651d17c55de369e9090f914
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-12-08 19:03:51 +0000

    try pretraining with slight data augmentation

--- scripts/validation.py
+++ scripts/validation.py
@@ -105,9 +105,9 @@ def main(config_file, weights_base_dir, output_dir, verbose, debug, seed, config
         config_updates['vgae_model']['params']['num_graph_attr'] = len(new_graph_attrs)
 
         # Training configs
-        config_updates['num_epochs'] = 2 if debug else 200
+        config_updates['num_epochs'] = 2 if debug else graphtrip_config['num_epochs']
         config_updates['mlp_lr'] = graphtrip_config['lr']
-        config_updates['num_z_samples'] = graphtrip_config['num_z_samples']
+        config_updates['num_z_samples'] = 3      # slight data augmentation
         config_updates['alpha'] = 0              # VGAE is frozen
         config_updates['vgae_lr'] = 0.0          # VGAE is frozen
         config_updates['reinit_pooling'] = False # Re-use trained, frozen pooling module
