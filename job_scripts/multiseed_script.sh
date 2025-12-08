9f8a256ea9bd97efe4d0f1332edfdd4bf98f5bed
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-12-08 19:30:57 +0000

    transfer vgae but train a new mlp on psilodep1

--- job_scripts/multiseed_script.sh
+++ job_scripts/multiseed_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=01:00:00
-#PBS -N repeat_control_mlp
+#PBS -l select=1:ncpus=4:mem=8gb
+#PBS -l walltime=08:00:00
+#PBS -N transfer_vgae
 #PBS -J 0-9
 
 module load anaconda3/personal
@@ -10,4 +10,4 @@ source activate graphtrip
 
 cd ~/projects/graphTRIP/scripts
 
-python ablation.py -s ${PBS_ARRAY_INDEX} -j 0 -v
+python transfer.py -s ${PBS_ARRAY_INDEX} -v
