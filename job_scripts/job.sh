1955c390684ab0a5c78a3166005571c7d2adab27
Hanna Tolle <hanna.m.tolle@gmail.com>
2025-12-16 23:20:25 +0100

    sanity check

--- job_scripts/job.sh
+++ job_scripts/job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #PBS -l select=1:ncpus=4:mem=4gb
-#PBS -l walltime=2:00:00
-#PBS -N pretrain_for_psilodep1
+#PBS -l walltime=8:00:00
+#PBS -N sanity_check
 
 module load anaconda3/personal
 source activate graphtrip
 
 cd ~/projects/graphTRIP/experiments
-python run_experiment.py train_jointly FileStorageObserver --jobid=0 --config_json='pretrain_for_psilodep1.json'
+python run_experiment.py train_cfrnet FileStorageObserver --jobid=0 --config_json='causal_graphtrip.json' --output_dir='outputs/causal_graphtrip/sanity_check/'
